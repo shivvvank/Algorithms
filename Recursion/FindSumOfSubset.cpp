54019cc4c4a6130362b8f2eeaaff605ea05111bf
Shivank Yadav <shivvvank@gmail.com>
2023-06-04 16:55:27 +0530

    find Sum of each Subset

---
+++ Recursion/FindSumOfSubset.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+void FindSubsetSum(int *a,int n,int i,int sum,vector<int>& sumsub)
+{
+    if(i==n)
+    {
+        sumsub.push_back(sum);
+        return;
+    }
+    FindSubsetSum(a,n,i+1,sum,sumsub);
+    sum+=a[i];
+    FindSubsetSum(a,n,i+1,sum,sumsub);
+}
+int main()
+{
+    int a[] = {2,6,3};
+    int n = sizeof(a)/sizeof(int);
+    vector<int> sumsubs;
+    FindSubsetSum(a,n,0,0,sumsubs);
+    for(auto i : sumsubs)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
